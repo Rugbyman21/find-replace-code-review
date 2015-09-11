7df19c05acd786cbabb26870b0a783c25f1ea9d2
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-11 12:06:03 -0700

    failed second spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,11 @@
 describe('replaceWord', function() {
 
-  it("return a word in a string and replace it with another word", function() {
+  it("return a word and replace it with another word", function() {
     expect(replaceWord('morning')).to.equal('coffee');
+  });
+
+  it("return a specific word in a string with another word", function(){
+    expect(replaceWord('Hello World')).to.equal('Hello Universe')
   });
 
 });
