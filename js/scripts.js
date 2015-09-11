9d4288062c134d2b7e11dd8a0cac245b5a6ff994
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-11 11:58:21 -0700

    pass first spec with a simple code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-var pigLatin = function(word) {
-
-
+var replaceWord = function(word) {
+//we want to change a word to a different word
+  var str = "morning";
+  var newWord = str.replace("morning", "coffee");
+// debugger;
+return newWord;
 };
 
 // $(document).ready(function() {
 //   $("form#ormfay").submit(function(event) {
 //     var input = $("input#word").val();
-//     var ordway = pigLatin(input);
+//     var word = pigLatin(input);
 //
-//     $("#title").text("Igpay Atinlay Anslatortray");
+//     $("#title").text("Replace Word");
 //     $("#label").text("Anslatetray aay ordway intoay igpay atinlay.");
-    // $("#button").text("Anslatetray!");
+    // $("#button").text(Submit);
 //
 //     $("#ordway").text(ordway);
 //     alert(ordway);
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('replaceWord', function() {
 
-  it("will take a word in a string and replace it with another word", function() {
+  it("return a word in a string and replace it with another word", function() {
     expect(replaceWord('morning')).to.equal('coffee');
   });
 
