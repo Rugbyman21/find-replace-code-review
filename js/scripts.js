bdad92330a158f2e57b2c6172c6ad6d64b249a70
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-11 10:10:42 -0700

    fail the first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,20 @@
 var pigLatin = function(word) {
 
-  var consonants = ['b','c','d','f', 'g','h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y',
-  'z'];
 
-  var vowels = ["a", "e", "i", "o", "u"];
-  var split1 = word.split('');
-  var firstLetter = split1[0];
-
-  if (vowels.indexOf(firstLetter) !== -1) {
-
-     split1.slice(1)
-     split1.push('ay');
-     split1 = split1.join('');
-
-     return split1;
-
-   };
 };
 
-$(document).ready(function() {
-  $("form#ormfay").submit(function(event) {
-    var input = $("input#word").val();
-    var ordway = pigLatin(input);
-
-    $("#title").text("Igpay Atinlay Anslatortray");
-    $("#label").text("Anslatetray aay ordway intoay igpay atinlay.");
-    $("#button").text("Anslatetray!");
-
-    $("#ordway").text(ordway);
-    alert(ordway);
-    $("#ordway").show();
-    event.preventDefault();
-  });
-});
-  // if (consonants.indexOf(firstLetter) !== -1) {
-  //
-  //   split1.push(firstletter);
-  //   split1 = split1.push('ay');
-  //   return split1;
-  // };
-
-
-
-
-
-// var split = word[0];
-// var word = 'apple';
+// $(document).ready(function() {
+//   $("form#ormfay").submit(function(event) {
+//     var input = $("input#word").val();
+//     var ordway = pigLatin(input);
+//
+//     $("#title").text("Igpay Atinlay Anslatortray");
+//     $("#label").text("Anslatetray aay ordway intoay igpay atinlay.");
+    // $("#button").text("Anslatetray!");
+//
+//     $("#ordway").text(ordway);
+//     alert(ordway);
+//     $("#ordway").show();
+//     event.preventDefault();
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,7 @@
-describe('pigLatin', function() {
+describe('replaceWord', function() {
 
-  it("will add ay to the end of a word starting with a vowel", function() {
-    expect(pigLatin('apple')).to.equal('appleay');
+  it("will take a word in a string and replace it with another word", function() {
+    expect(replaceWord('morning')).to.equal('coffee');
   });
-
-  it("will describe words that start with one consonant", function() {
-    expect(pigLatin('banana')).to.equal('ananabay');
-  });
-  
 
 });
